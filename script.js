eb9191f626769801893f7fcd376f57a5d4e0252a
Irtaza148 <dev.irtazanaveed@gmail.com>
2024-12-14 15:18:44 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+//1
+var a = 2;
+var b = ++a * 2;
+console.log("b =", b);
+//2
+var x = 5;
+var y = x-- + 2;
+console.log("y =", y);
+//3
+var x = 3;
+var y = ++x + x++ + ++x;
+console.log("y =", y);
+//4
+var m = 2;
+var n = ++m * m++ * --m;
+console.log("n =", n);
+//5
+var a = 3;
+var b = 5;
+var result = ++a + b-- - a++ + --b;
+console.log("Result =", result);
+//6
+var m = 2;
+var n = 4;
+var p = m++ + ++n - --m + n--;
+console.log("p =", p);
+//7
+var a = 5;
+var b = 3;
+var c = 2;
+var d = 7;
+var result = ++a * (b-- + c) / --d;
+console.log("Result =", result);
+//8
+var m = 2;
+var n = 3;
+var o = 4;
+var result = m++ * (--n + m) / (o-- - n);
+console.log("m =", m, "o =", o, "n = ", n, "Result = ", result);
